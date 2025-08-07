2bb4ac189a3befb4b57e7f629e492cbe10bf99b4
WrathfulSpatula <stranoj@gmail.com>
2025-08-07 14:08:58 -0400

    Hypothetical near-Clifford optimization

--- include/qstabilizerhybrid.hpp
+++ include/qstabilizerhybrid.hpp
@@ -342,6 +342,8 @@ protected:
         --ancillaCount;
     }
 
+    void PruneAncillae(bool gaussian);
+
     real1_f ApproxCompareHelper(
         QStabilizerHybridPtr toCompare, bool isDiscreteBool, real1_f error_tol = TRYDECOMPOSE_EPSILON);
 
