264cbddfc5d5c343a91717b630f80bd2839083f6
WrathfulSpatula <stranoj@gmail.com>
2025-08-15 20:12:44 -0400

    Simplify/optimize

--- scripts/maxcut.py
+++ scripts/maxcut.py
@@ -12,75 +12,46 @@ import networkx as nx
 
 # By Gemini (Google Search AI)
 def int_to_bitstring(integer, length):
-    return bin(integer)[2:].zfill(length)
+    return (bin(integer)[2:].zfill(length))[::-1]
 
 
-def separation_metric(adjacency, state_int, n_qubits):
-    like_count = 0
-    total_edges = 0
-    for i, neighbors in adjacency.items():
-        for j in neighbors:
-            if j > i:
-                bit_i = (state_int >> (n_qubits - 1 - i)) & 1
-                bit_j = (state_int >> (n_qubits - 1 - j)) & 1
-                like_count += -1 if bit_i == bit_j else 1
-                total_edges += 1
+def best_cut_in_weight(args):
+    nodes, edges, m = args
+    n = len(nodes)
+    best_val = -1
+    best_state = None
 
-    return like_count / total_edges
+    for combo in itertools.combinations(nodes, m):
+        # Represent state as an integer
+        state = 0
+        for pos in combo:
+            state |= 1 << pos
 
+        # Compute cut size using bitwise ops
+        cut_val = 0
+        for u, v in edges:
+            if ((state >> u) & 1) != ((state >> v) & 1):
+                cut_val += 1
 
-def best_separation(args):
-    adjacency, qubits, m = args
-    n_qubits = len(qubits)
-    combo_count = math.factorial(n_qubits) // (math.factorial(m) * math.factorial(n_qubits - m))
+        if cut_val > best_val:
+            best_val = cut_val
+            best_state = state
 
-    if math.log2(combo_count) > 25:
-        best_separation = 0
-        best_state_int = 0
-        for combo in itertools.combinations(qubits, m):
-            state_int = sum((1 << pos) for pos in combo)
-            sep = (1.0 + separation_metric(adjacency, state_int, n_qubits)) / 2.0
-            if sep > best_separation:
-                best_separation = sep
-                best_state_int = state_int
-
-        return best_state_int
-
-    state_ints = [sum((1 << pos) for pos in combo) for combo in itertools.combinations(qubits, m)]
-    like_count = [0] * len(state_ints)
-    for i, neighbors in adjacency.items():
-        for j in neighbors:
-            if j > i:
-                for k in range(len(state_ints)):
-                    state_int = state_ints[k]
-                    bit_i = (state_int >> (n_qubits - 1 - i)) & 1
-                    bit_j = (state_int >> (n_qubits - 1 - j)) & 1
-                    like_count[k] += -1 if bit_i == bit_j else 1
-
-    return state_ints[like_count.index(max(like_count))]
+    return best_state
 
 
-def maxcut_by_hamming_weight(G, n_qubits):
-    qubits = list(range(n_qubits))
-    G_dol = nx.to_dict_of_lists(G)
+def maxcut_by_hamming_weight(G):
+    nodes = G.nodes
+    edges = G.edges()
+    n_qubits = len(nodes)
     samples = []
     with multiprocessing.Pool(processes=os.cpu_count()) as pool:
         args = []
         for m in range(1, n_qubits):
-            args.append((G_dol, qubits, m))
-        samples = pool.map(best_separation, args)
+            args.append((nodes, edges, m))
+        samples = pool.map(best_cut_in_weight, args)
 
     return samples
-
-
-def graph_to_J(G, n_nodes):
-    """Convert networkx.Graph to J dictionary for TFIM."""
-    J = np.zeros((n_nodes, n_nodes))
-    for u, v, data in G.edges(data=True):
-        weight = data.get("weight", 1.0)  # Default weight = 1.0
-        J[u, v] = -weight
-
-    return J
 
 
 def evaluate_cut(G, bitstring_int):
@@ -109,7 +80,7 @@ if __name__ == "__main__":
     # Qubit count
     n_qubits = G.number_of_nodes()
 
-    meas = set(maxcut_by_hamming_weight(G, n_qubits))
+    meas = maxcut_by_hamming_weight(G)
 
     best_value = -1
     best_solution = None
