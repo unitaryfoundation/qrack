697ff96c730f27fa7d1ed4502b1044b13598ea1a
WrathfulSpatula <stranoj@gmail.com>
2026-08-31 15:56:30 -0400

    OpenCL/CUDA parity

--- src/qengine/cuda.cu
+++ src/qengine/cuda.cu
@@ -3304,9 +3304,6 @@ std::shared_ptr<complex> QEngineCUDA::AllocStateVec(bitCapIntOcl elemCount, bool
         return nullptr;
     }
 
-#if defined(__ANDROID__)
-    return std::shared_ptr<complex>(elemCount);
-#else
     // elemCount is always a power of two, but might be smaller than QRACK_ALIGN_SIZE
     size_t allocSize = sizeof(complex) * (size_t)elemCount;
     if (allocSize < QRACK_ALIGN_SIZE) {
@@ -3320,8 +3317,6 @@ std::shared_ptr<complex> QEngineCUDA::AllocStateVec(bitCapIntOcl elemCount, bool
         (complex*)_aligned_malloc(allocSize, QRACK_ALIGN_SIZE), [](complex* c) { _aligned_free(c); });
 #else
     return std::shared_ptr<complex>((complex*)aligned_alloc(QRACK_ALIGN_SIZE, allocSize), [](complex* c) { free(c); });
-#endif
-
 #endif
 }
 
